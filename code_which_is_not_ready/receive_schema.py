988ac2809a2b16f554478e16e995ff4b913b0b39
Sasha_Drozdov <drsasha7@mail.ru>
2018-11-25 21:31:56 +0300

    добавил еще незаконченого кода

--- code_which_is_not_ready/receive_schema.py
+++ code_which_is_not_ready/receive_schema.py
@@ -1,4 +1,39 @@
+schema_folder = []
+tests_folder = []
+is_schemas_read = False
+is_tests_read = False
 
+#void-функции работы с информацией
+
+def read_schemas():
+    global schema_folder, is_schemas_read
+    t = True
+    fin = open("schema_folder", "r")
+    
+    #тут считывание, но мы еще не согласовали формат
+    
+    fin.close()
+
+def read_tests():
+    global
+
+def write_schemas():
+    global schema_folder, t
+    fout = open("schema_folder", "w")
+    
+    #тут запись, но мы еще не согласовали формат
+    
+    fout.close()
+
+##конец работы с информацией
+
+#это просто добавление схемы в ящик
+
+def save_schema(user, task, schema):
+    global schema_folder, t
+    #это костыль на то, инициализирован ли ящик
+    if not(t):
+        read()
 
 def to_string(file_name):
     #list of str
@@ -14,14 +49,22 @@ def to_string(file_name):
     
     return answer
 
+def lexical_check(schema):
+    #здесь умная компиляция
+    return True
+    
+def test_check(user, task, schema):
+    #проверка на тестах, которую мне сейчас лань писать
+    return
+
 
 def receive_schema(user, task, file_name):
     #list of str
-    s = to_string(file_name)
+    schema = to_string(file_name)
     #bool
-    result = lexical_check(s)
+    result = lexical_check(schema)
     if (result != True): # схема некорректно написана
         return "Compilation Error"
-    test_check(s)
-    save(user, task, file_name)
+    test_check(user, task, schema)
+    save(user, task, schema)
     return "Accepted For Testing"
