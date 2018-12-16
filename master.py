44161800c86311cc476e8347d23c8612da101ef4
Sasha_Drozdov <drsasha7@mail.ru>
2018-12-16 23:36:59 +0300

    тут надо еще подумать

--- master.py
+++ master.py
@@ -2,15 +2,19 @@ import folder_working as FW
 
 #тут всякие глобальные переменные
 
-#submits - map<string, map<string, int> >, где submits[user][task] - число посылок юзера user по задаче task 
+#submits - map<string, map<string, int> > , где submits[user][task] - число посылок юзера user по задаче task 
+#в пред строчке лажа - обязательно разобраться
 
 submits = dict()
 own_dir = "/.submits"
 
 def save():
     global submits, own_dir
-    a = 0
-    #сохранение
+    for user in submits:
+        FW.create_folder(own_dir + FW.make_path(user))
+        for task in submits[user]:
+            FW.create_folder(own_dir + FW.make_path(user) + FW.make_path(task))
+            #создадим файлы
     
 def read():
     global submits, own_dir
@@ -24,7 +28,7 @@ def read():
         tasks = FW.getdirs(now)
         for task in tasks:
             now2 = now + FW.make_path(task)
-            submits[user][task] = len(FW.getdirs(now2))
+            #считаем файлы
     
 def new_user(user):
     if user not in submits:
