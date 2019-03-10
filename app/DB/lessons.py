c724ceca9446daa3ab9c74b9b7d89e3f8d40fb28
Sasha_Drozdov <drsasha7@mail.ru>
2019-03-10 13:46:04 +0300

    дописал lessons.py

--- app/DB/lessons.py
+++ app/DB/lessons.py
@@ -1,27 +1,67 @@
 ## -*- coding utf-8 -*-
 
-import folder_working as FW
+try:
+    import folder_working as FW
+    import problems as P
+except ImportError:
+    from app.DB import folder_working as FW
+    from app.DB import problems as P
 
-own_folder = "/.lessons"
+own_dir = "/.lessons"
+
+def lesson_exists(name):
+    name = FW.make_path(name)
+    return FW.file_exists(own_dir + name)
+
+def lessons_list():
+    return FW.getdirs(own_dir)
 
 def add_lesson(name):
+    if (lesson_exists(name)):
+        return
     name = FW.make_path(name)
-    FW.create_file(own_folder, name)
+    FW.create_file(own_dir, name)
     
 def add_task(lesson_name, task_name):
-    lesson_name = make_path(lesson_name)
-    if folder_exists(own_path + lesson_name) and folder_exists("/.tasks" + FW.make_path(task_name)):
+    lesson_name = FW.make_path(lesson_name)
+    if lesson_exists(lesson_name) and P.task_exists(task_name):
         FW.add_text(own_dir, lesson_name, [task_name + "\n"])
 
+def tasks_list(lesson):
+    lesson = FW.make_path(lesson)
+    ans = []
+    for i in FW.to_string(own_dir + lesson):
+        ans.append(i.strip())
+    return ans
+
+def add_task_to_pos(lesson_name, task_name, pos):
+    if lesson_exists(lesson_name) and P.task_exists(task_name):
+        pos -= 1
+        lst = tasks_list(lesson_name)
+        ans = []
+        if len(lst) < pos:
+            pos = len(lst)
+        for i in range(pos):
+            ans.append(lst[i] + "\n")
+        ans.append(task_name + "\n")
+        for i in range(pos, len(lst)):
+            ans.append(lst[i] + "\n")
+        FW.write_text(own_dir, FW.make_path(lesson_name), ans)
+        
+
 def remove_task(lesson_name, task_name):
-    lesson_name = make_path(lesson_name)
-    if folder_exists(own_path + lesson_name) and folder_exists("/.tasks" + FW.make_path(task_name)):
-        tasks = to_string(own_path + lesson_name)
+    lesson_name = FW.make_path(lesson_name)
+    if lesson_exists(lesson_name) and P.task_exists(task_name):
+        tasks = tasks_list(lesson_name)
         new_tasks = []
+        cnt = 1
         for i in tasks:
-            if i != taskname:
+            if i != task_name and cnt:
+                cnt = 0
                 new_tasks.append(i + "\n")
         FW.write_text(own_dir, lesson_name, new_tasks)
                 
+def init():
+    FW.make_fodler(own_dir)
         
         
